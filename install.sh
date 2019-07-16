a9956deb62c08e8a35486454f8168087d5595285
Artem Eger <11612812+yuomii@users.noreply.github.com>
2019-07-16 23:08:35 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ sudo rm gradle-4.10.2-bin.zip
 echo "Clone APEX Core"
 git clone https://github.com/APEX-Network/APEX-Blockchain-Core.git
 cd APEX-Blockchain-Core
-git checkout testnet1
+git checkout mainnet
 git pull
 echo "Build Core"
 ~/gradle/gradle-4.10.2/bin/gradle shadowJar
