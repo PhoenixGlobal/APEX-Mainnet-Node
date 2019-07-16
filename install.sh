075e7508e90095ce5283b76bc83bb371ecbcea37
Artem Eger <11612812+yuomii@users.noreply.github.com>
2019-07-16 15:15:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-echo "Install Git"
-sudo apt-get install git -y
 echo "Add java repo"
-apt-get install software-properties-common -y
+sudo apt install software-properties-common -y
 wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
 sudo add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
-sudo apt-get update -y
-sudo apt-get install unzip -y
+sudo apt update -y
+sudo apt install unzip -y
 echo "Install java"
-sudo apt-get install adoptopenjdk-12-hotspot -y
+sudo apt install adoptopenjdk-12-hotspot -y
 echo "Get gradle binary"
 wget https://services.gradle.org/distributions/gradle-4.10.2-bin.zip
 echo "Install gradle"
@@ -35,6 +33,10 @@ mkdir libs
 cd ..
 cp apex-blockchain-core.jar APEX-Blockchain-CLI/libs/APEX-Blockchain-Core-0.9.1.jar
 cd APEX-Blockchain-CLI
+echo "Build Client"
 ~/gradle/gradle-4.10.2/bin/gradle shadowJar
 cd ..
 cp APEX-Blockchain-CLI/build/libs/APEX-Blockchain-CLI-0.9.1.jar apex-blockchain-cli.jar
+echo ""
+echo "Installed APEX-Core and APEX-Cli successfully"
+echo ""
