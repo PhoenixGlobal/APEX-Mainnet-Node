312d91c6813ce38477750f7a8bf1ac98e1e845dd
Artem Eger <11612812+yuomii@users.noreply.github.com>
2019-07-17 15:01:22 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar -Xms512m -Xmx10G -XX:+UnlockExperimentalVMOptions -XX:+UseZGC apex-blockchain-core.jar settings.conf
+java -jar -Xms512m -Xmx12G -XX:+UnlockExperimentalVMOptions -XX:+UseZGC apex-blockchain-core.jar settings.conf
