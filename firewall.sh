3155b13960c960daf6ee9b2db2f3c92d8a0c8430
Artem Eger <11612812+yuomii@users.noreply.github.com>
2019-07-19 10:08:45 +0200

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -1,6 +1,6 @@
 sudo iptables -I INPUT 1 -p tcp --dport 22 -j ACCEPT
-sudo iptables -I INPUT 2 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -I INPUT 2 -m conntrack --ctstate NEW,ESTABLISHED,RELATED -j ACCEPT
 sudo iptables -I INPUT 3 -p tcp --dport 9090 -j ACCEPT
-sudo iptables -I INPUT 4 -p tcp --sport 9090 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -I INPUT 4 -p tcp --sport 9090 -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -I INPUT 5 -s localhost -j ACCEPT
 sudo iptables -I INPUT 6 -j DROP
