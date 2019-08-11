fd81d579303ab86439d03446b7a11819653822e8
Artem Eger <11612812+yuomii@users.noreply.github.com>
2019-08-11 12:11:32 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar -Xms512m -Xmx12G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=6000 -XX:G1HeapRegionSize=32M apex-blockchain-core.jar settings.conf
+java -jar -Xms512m -Xmx12G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=6500 -XX:G1HeapRegionSize=32M apex-blockchain-core.jar settings.conf
